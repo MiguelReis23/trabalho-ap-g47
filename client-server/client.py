5db549a9bdbec18c3f1242a371118d65b4400c36
Miguel Reis <r315miguel@gmail.com>
2022-05-30 10:43:43 +0100

    Adiçao de comentarios

--- client-server/client.py
+++ client-server/client.py
@@ -13,42 +13,42 @@ from Crypto.Cipher import AES
 # Função para encriptar valores a enviar em formato json com codificação base64
 # return int data encrypted in a 16 bytes binary string coded in base64
 def encrypt_intvalue (cipherkey, data):
-	if cipherkey is None:
+	if cipherkey is None: #check if cipherkey exists
 		return data
-	cipher = AES.new(cipherkey, AES.MODE_ECB)
-	dataencrypted= cipher.encrypt(bytes("%16d"%(data), "utf8"))
-	dataenctyptedtosend= base64.b64encode(dataencrypted)
-	return str(dataenctyptedtosend, "utf8")
+	cipher = AES.new(cipherkey, AES.MODE_ECB) #create cipher
+	dataencrypted= cipher.encrypt(bytes("%16d"%(data), "utf8")) #encrypt data
+	dataencryptedtosend= base64.b64encode(dataencrypted) #encode data
+	return str(dataencryptedtosend, "utf8") #return data encrypted
 	
 
 
 # Função para desencriptar valores recebidos em formato json com codificação base64
 # return int data decrypted from a 16 bytes binary strings coded in base64
 def decrypt_intvalue (cipherkey, data):
-	if cipherkey is None:
+	if cipherkey is None: #check if cipherkey exists
 		return data
-	cipher= AES.new(cipherkey, AES.MODE_ECB)
-	datadecoded= base64.b64decode(data)
-	datadecrypted= cipher.decrypt(datadecoded)
-	return int(str(datadecrypted, "utf8"))
+	cipher= AES.new(cipherkey, AES.MODE_ECB) #create cipher
+	datadecoded= base64.b64decode(data) #decode data
+	datadecrypted= cipher.decrypt(datadecoded) #decrypt data
+	return int(str(datadecrypted, "utf8")) #return decrypted data
 
 
 # verify if response from server is valid or is an error message and act accordingly
 def validate_response (client_sock, response):
-	if "error" in response:
-		print ("ERROR - %s" % (response["error"]))
-		client_sock.close ()
-		sys.exit (3)
+	if "error" in response: # check if response is an error message
+		print ("ERROR - %s" % (response["error"])) # print error message
+		client_sock.close () # close socket
+		sys.exit (3) # exit with error
 	return True
 
 
 # process QUIT operation
 def quit_action (client_sock):
-	response = sendrecv_dict (client_sock, {"op": "QUIT"})
-	if "error" in response:
-		print ("ERROR -  %s" % (response["error"]))
-		client_sock.close ()
-		sys.exit (4)
+	response = sendrecv_dict (client_sock, {"op": "QUIT"}) # send QUIT operation
+	if "error" in response: # check if response is an error message
+		print ("ERROR -  %s" % (response["error"])) # print error message
+		client_sock.close () # close socket
+		sys.exit (4) # exit with error
 	return None
 
 
@@ -69,48 +69,48 @@ def quit_action (client_sock):
 # Suporte da execução do cliente
 #
 def run_client (client_sock, client_id):
-	cipherkey=None
-	cipherkeytosend=None
-	op= ""
-	numbers=[]	
+	cipherkey=None #cipherkey is None by default
+	cipherkeytosend=None #cipherkeytosend is None by default
+	op= "" #op is empty by default
+	numbers=[] #numbers is empty by default
 	
-	encrypt= input("CLIENT - Do you want to encrypt the numbers? (y/n): ")
-	while encrypt.upper() != "Y" and encrypt.upper() != "N":
-		print ("ERROR -  Invalid option")
-		encrypt= input("CLIENT - Do you want to encrypt the numbers? (y/n): ")
+	encrypt= input("CLIENT - Do you want to encrypt the numbers? (y/n): ") #ask if user wants to encrypt numbers
+	while encrypt.upper() != "Y" and encrypt.upper() != "N": #check if user input is valid
+		print ("ERROR -  Invalid option") #print error message if user input is invalid
+		encrypt= input("CLIENT - Do you want to encrypt the numbers? (y/n): ") #ask again if user wants to encrypt numbers
 	if encrypt.upper() == "Y" :
-		cipherkey= os.urandom(16)
-		cipherkeytosend= str(base64.b64encode(cipherkey), "utf8")
+		cipherkey= os.urandom(16) #create random cipherkey
+		cipherkeytosend= str(base64.b64encode(cipherkey), "utf8") #encode cipherkey
 	
      
-	response= sendrecv_dict (client_sock, {"op": "START", "client_id": client_id, "cipher": cipherkeytosend})
-	validate_response (client_sock, response)
-	print ("Operation START successfully executed" )
+	response= sendrecv_dict (client_sock, {"op": "START", "client_id": client_id, "cipher": cipherkeytosend}) #send START operation
+	validate_response (client_sock, response) #verify if response is valid
+	print ("Operation START successfully executed" ) #print message if operation is successful
  
-	while op.upper() != "QUIT" or op!= "STOP":
-		op= input("CLIENT - Enter operation(QUIT, STOP) or number: ")
+	while op.upper() != "QUIT" or op!= "STOP": #check if user wants to quit or stop
+		op= input("CLIENT - Enter operation(QUIT, STOP) or number: ") #ask user for operation
 		
-		if op.upper() == "STOP":
-			response= sendrecv_dict (client_sock, {"op": "STOP"})
-			validate_response (client_sock, response)
-			print ("Operation STOP successfully executed" )
-			print ("CLIENT - Numbers: {}, Min: {}, Max: {}".format(numbers, min(numbers), max(numbers)))
+		if op.upper() == "STOP": #check if user wants to stop
+			response= sendrecv_dict (client_sock, {"op": "STOP"}) #send STOP operation
+			validate_response (client_sock, response) #verify if response is valid
+			print ("Operation STOP successfully executed" ) #print message if operation is successful
+			print ("CLIENT - Numbers: {}, Min: {}, Max: {}".format(numbers, min(numbers), max(numbers))) #print numbers and min and max
 			break
-		elif op.upper() == "QUIT":
-			quit_action (client_sock)
-			print ("Operation QUIT successfully executed" )
-			break
+		elif op.upper() == "QUIT": #check if user wants to quit
+			quit_action (client_sock) #process QUIT operation
+			print ("Operation QUIT successfully executed" ) #print message if operation is successful
+			break 
 		else:
 			try:
-				number=int(op)
-				numbers.append(number)
-			except ValueError:
-				print ("ERROR - Invalid number or operation")
+				number=int(op) #try to convert user input to int
+				numbers.append(number) #add number to list
+			except ValueError: #check if user input is not a number
+				print ("ERROR - Invalid number or operation") #print error message
 				continue
 				
-			response= sendrecv_dict (client_sock, {"op": "NUMBER", "number": encrypt_intvalue(cipherkey,number)})
-			validate_response (client_sock, response)
-			print ("CLIENT - Number {} sent".format(number))
+			response= sendrecv_dict (client_sock, {"op": "NUMBER", "number": encrypt_intvalue(cipherkey,number)}) #send NUMBER operation
+			validate_response (client_sock, response) #verify if response is valid
+			print ("CLIENT - Number {} sent".format(number)) #print message if operation is successful
 
 
 
@@ -119,25 +119,25 @@ def run_client (client_sock, client_id):
 def main():
 	# validate the number of arguments and eventually print error message and exit with error
 	# verify type of of arguments and eventually print error message and exit with error
-	if len(sys.argv) <3 or len(sys.argv) > 4:
-		print ("ERROR - Usage: python3 {} <client_id> <port> <host>".format(sys.argv[0]))
-		sys.exit (1)
-	if not sys.argv[2].isdigit():
-		print ("ERROR - Invalid port")
-		sys.exit (2)
+	if len(sys.argv) <3 or len(sys.argv) > 4: #check if number of arguments is valid
+		print ("ERROR - Usage: python3 {} <client_id> <port> <host>".format(sys.argv[0])) #print error message and correct usage
+		sys.exit (1) #exit with error
+	if not sys.argv[2].isdigit(): #check if port is a number
+		print ("ERROR - Invalid port") #print error message
+		sys.exit (2) #exit with error
 	
-	client_id = sys.argv[1]
-	port = int(sys.argv[2])
-	hostname = sys.argv[3] if len(sys.argv) > 3 else "localhost"
+	client_id = sys.argv[1] #get client id from arguments
+	port = int(sys.argv[2]) #get port from arguments
+	hostname = sys.argv[3] if len(sys.argv) > 3 else "localhost" #get hostname from arguments if exists, otherwise use localhost
 
 	# create a socket and connect to the server
-	client_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-	client_sock.connect ((hostname, port))
+	client_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM) #create socket
+	client_sock.connect ((hostname, port)) #connect to server
 
-	run_client (client_sock, client_id)
+	run_client (client_sock, client_id) #process client
 
-	client_sock.close ()
-	sys.exit (0)
+	client_sock.close () #close socket
+	sys.exit (0) #exit with success
 
 if __name__ == "__main__":
 	main()
--- client-server/server.py
+++ client-server/server.py
@@ -16,7 +16,7 @@ users = {}
 
 # return the client_id of a socket or None
 
-#
+#find the client_id if it exists
 def find_client_id(client_sock):
 	for client_id in users:
 		if users[client_id]["socket"] == client_sock:
@@ -26,10 +26,11 @@ def find_client_id(client_sock):
 
 # Função para encriptar valores a enviar em formato json com codificação base64
 # return int data encrypted in a 16 bytes binary string and coded base64
+
 def encrypt_intvalue(client_id, data):
-	cipherkey= users[client_id]["cipher"]
+	cipherkey= users[client_id]["cipher"] #check if the user has a cipher 
 	if cipherkey is None or not cipherkey: return data
-	
+	#if the user has a cipher, encrypt the data
 	cipher = AES.new(cipherkey, AES.MODE_ECB)
 	dataencrypted= cipher.encrypt(bytes("%16d"%(data), "utf8"))
 	dataencryptedtosend= base64.b64encode(dataencrypted)
@@ -39,13 +40,12 @@ def encrypt_intvalue(client_id, data):
 # Função para desencriptar valores recebidos em formato json com codificação base64
 # return int data decrypted from a 16 bytes binary string and coded base64
 def decrypt_intvalue(client_id, data):
-	cipherkey= users[client_id]["cipher"]
+	cipherkey= users[client_id]["cipher"]#check if the user has a cipher
 	if cipherkey is None or not cipherkey: return data
-	
+	#if the user has a cipher, decrypt the data
 	cipher = AES.new(cipherkey, AES.MODE_ECB)
 	datadecoded= base64.b64decode(data)
 	datadecrypted= cipher.decrypt(datadecoded)
-	
 	return int(str(datadecrypted, "utf8"))
 
 
@@ -60,6 +60,8 @@ def decrypt_intvalue(client_id, data):
 # { op = "QUIT" , status }
 # { op = "NUMBER", status }
 # { op = "STOP", status, min, max }
+
+#function to handle the incoming messages
 def operation(request):
 	if request["op"] == "START":
 		return "START"
@@ -70,11 +72,10 @@ def operation(request):
 	elif request["op"] == "STOP":
 		return "STOP"
 
-#
+
 # Suporte de descodificação da operação pretendida pelo cliente
-#
 
-
+#from input run the functions
 def new_msg(client_sock):
 	msg = recv_dict(client_sock)
 	if not "op" in msg: return False
@@ -99,25 +100,26 @@ def new_msg(client_sock):
 # send the response to the client
 
 
-#
+
 # Suporte da criação de um novo jogador - operação START
-#
+
 def new_client(client_sock, request):
-	if not "client_id" in request:
+	if not "client_id" in request: #check if the client_id is in the request
 		return {"op": "START", "status": False,"error":"client_id not found"}
-	if not "cipher" in request:
+	if not "cipher" in request: #check if the cipher is in the request
 		return {"op": "START", "status": False,"error":"cipher not found"}
 	
 	client_id= request["client_id"]
 	cipher= request["cipher"]
 	
 	
-	if  cipher: cipherkey = base64.b64decode(cipher)
+	if  cipher: cipherkey = base64.b64decode(cipher) 
 	else: cipherkey= None
-	
+ 
+	#check if the client_id is already in the dictionary
 	if client_id in users: return {"op": "START", "status": False,"error":"client_id already exists"}
 	
-	users[client_id]= {"socket": client_sock, "cipher": cipherkey, "numbers": []}
+	users[client_id]= {"socket": client_sock, "cipher": cipherkey, "numbers": []}#add the client to the dictionary
 	print("SERVER - Client {} conected".format(client_id))
 	return {"op": "START", "status": True}
 
@@ -133,10 +135,11 @@ def new_client(client_sock, request):
 #
 # Suporte da eliminação de um cliente
 #
+
 def clean_client(client_sock):
-	client_id = find_client_id(client_sock)
-	if client_id in users:
-		del users[client_id]
+	client_id = find_client_id(client_sock) #find the client_id
+	if client_id in users: #check if the client_id is in the dictionary
+		del users[client_id] #delete the client from the dictionary
 	return None
 # obtain the client_id from his socket and delete from the dictionary
 
@@ -144,12 +147,13 @@ def clean_client(client_sock):
 #
 # Suporte do pedido de desistência de um cliente - operação QUIT
 #
+
 def quit_client(client_sock, request): 
-  client_id = find_client_id(client_sock)
-  if not client_id: return {"op": "QUIT", "status": False,"error":"client_id not found"}
+  client_id = find_client_id(client_sock)#find the client_id
+  if not client_id: return {"op": "QUIT", "status": False,"error":"client_id not found"} #return error if the client_id is not in the dictionary
   
-  del users[client_id]
-  print("SERVER - Client {} gave up on connection".format(client_id))
+  del users[client_id] #delete the client from the dictionary
+  print("SERVER - Client {} gave up on connection".format(client_id)) #print the quit message
   return {"op": "QUIT", "status": True}
 
 # obtain the client_id from his socket
@@ -163,10 +167,10 @@ def quit_client(client_sock, request):
 # Suporte da criação de um ficheiro csv com o respectivo cabeçalho
 #
 def create_file():
-	if not os.path.isfile("report.csv"):
+	if not os.path.isfile("report.csv"): #check if the file report.csv exists
 		file = open("report.csv", "w")
-		writer = csv.DictWriter(file, fieldnames=["client_id", "numbers", "minimum", "maximum"])
-		writer.writeheader()
+		writer = csv.DictWriter(file, fieldnames=["client_id", "numbers", "minimum", "maximum"]) #format report.csv header
+		writer.writeheader() #write the header
 		file.close()
 	return None
 # create report csv file with header
@@ -176,9 +180,9 @@ def create_file():
 # Suporte da actualização de um ficheiro csv com a informação do cliente e resultado
 #
 def update_file(client_id, result):
-	file = open("report.csv", "a")
-	writer= csv.DictWriter(file, fieldnames=["client_id", "numbers", "minimum", "maximum"])
-	writer.writerow({"client_id": client_id, "numbers": len(result["numbers"]), "minimum": result["minimum"], "maximum": result["maximum"]})
+	file = open("report.csv", "a") #open the file in append mode
+	writer= csv.DictWriter(file, fieldnames=["client_id", "numbers", "minimum", "maximum"]) #format report.csv header
+	writer.writerow({"client_id": client_id, "numbers": len(result["numbers"]), "minimum": result["minimum"], "maximum": result["maximum"]}) #write the row of the client
 	return None
 # update report csv file with the result from the client
 
@@ -187,13 +191,13 @@ def update_file(client_id, result):
 # Suporte do processamento do número de um cliente - operação NUMBER
 #
 def number_client(client_sock, request):
-	client_id = find_client_id(client_sock)
-	if not client_id: return {"op": "NUMBER", "status": False,"error":"client_id not found"}
-	if not "number" in request: return {"op": "NUMBER", "status": False,"error":"number not found"}
-	number= request["number"]
-	number= decrypt_intvalue(client_id, number)
-	users[client_id]["numbers"].append(number)
-	print("SERVER - Client {} sent number {}".format(client_id, number))
+	client_id = find_client_id(client_sock) #find the client_id
+	if not client_id: return {"op": "NUMBER", "status": False,"error":"client_id not found"} #return error if the client_id is not in the dictionary
+	if not "number" in request: return {"op": "NUMBER", "status": False,"error":"number not found"} #return error if the number is not in the request
+	number= request["number"] 
+	number= decrypt_intvalue(client_id, number) #decrypt the number
+	users[client_id]["numbers"].append(number) #add the number to the client
+	print("SERVER - Client {} sent number {}".format(client_id, number)) #print the number sent by the client
 	return {"op": "NUMBER", "status": True}
 	
 # obtain the client_id from his socket
@@ -205,23 +209,23 @@ def number_client(client_sock, request):
 # Suporte do pedido de terminação de um cliente - operação STOP
 #
 def stop_client(client_sock):
-	client_id= find_client_id(client_sock)
+	client_id= find_client_id(client_sock) #find the client_id
 	
-	if not client_id: return {"op": "STOP", "status": False,"error":"client_id not found"}
+	if not client_id: return {"op": "STOP", "status": False,"error":"client_id not found"} #return error if the client_id is not in the dictionary
 	
-	numbers= users[client_id]["numbers"]
+	numbers= users[client_id]["numbers"] #get the numbers from the client
 	
-	if len(numbers)==0: return {"op": "STOP", "status": False,"error":"no numbers found"}
+	if len(numbers)==0: return {"op": "STOP", "status": False,"error":"no numbers found"} #return error if the client has not sent any number
 	
-	minNumber= min(numbers)
-	maxNumber= max(numbers)
+	minNumber= min(numbers) #get the minimum number
+	maxNumber= max(numbers) #get the maximum number
 	
-	result= {"numbers": numbers, "minimum": minNumber, "maximum": maxNumber}
-	update_file(client_id, result)
+	result= {"numbers": numbers, "minimum": minNumber, "maximum": maxNumber} #create the result
+	update_file(client_id, result) #update the report file
 	
-	del users[client_id]
+	del users[client_id] #delete the client from the dictionary
 	print("SERVER - Client {} disconnected.".format(client_id))
-	print("SERVER - Results: {} numbers sent, minimum: {}, maximum: {}".format(len(numbers), minNumber, maxNumber))
+	print("SERVER - Results: {} numbers sent, minimum: {}, maximum: {}".format(len(numbers), minNumber, maxNumber)) #print the results
 	
 	return {"op": "STOP", "status": True, "minimum": minNumber, "maximum": maxNumber}
 
@@ -235,33 +239,38 @@ def stop_client(client_sock):
 def main():
 	# validate the number of arguments and eventually print error message and exit with error
 	# verify type of of arguments and eventually print error message and exit with error
+	
+ 	#check if the user has entered the correct number of arguments
 	if len(sys.argv) < 2:
 		print("ERROR - Usage: python3 {} <port>".format(sys.argv[0]))
 		sys.exit(1)
 	
-	port = sys.argv[1]
+	port = sys.argv[1] #get the port from the arguments
 	
+	#check if the port is a number
 	if not port.isdigit():
 		print("SERVER - Port must be a number")
 		sys.exit(1)
 	else:
 		port=int(port)
 	
+ 	#check if the port is in the correct range
 	if port<1024 or port>65535:
 		print("SERVER - Port must be between 1024 and 65535")
 		sys.exit(1)
 
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind(("127.0.0.1", port))
-	server_socket.listen(10)
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create the server socket
+	server_socket.bind(("127.0.0.1", port)) #bind the server socket to the port
+	server_socket.listen(10) #listen for connections
 
-	clients = []
-	create_file()
-	print("SERVER - Waiting for connections at port {}".format(port))
+	clients = [] #create a list of clients
+	create_file() #create the report file
+	print("SERVER - Waiting for connections at port {}".format(port)) #print that the server is waiting for connections
+	
 	while True:
 		
 		try:
-			available = select.select([server_socket] + clients, [], [])[0]
+			available = select.select([server_socket] + clients, [], [])[0] 
 		except ValueError:
 			# Sockets may have been closed, check for that
 			for client in clients:
